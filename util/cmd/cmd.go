508d09c6af4b98507cc04370d6b20a494c851c20
guantingwei <guantingwei@sixents.com>
2020-06-18 20:53:13 +0800

    新增 cmd.AddCommand 函数

--- util/cmd/cmd.go
+++ util/cmd/cmd.go
@@ -16,16 +16,15 @@ import (
 
 var (
 	AppName string = filepath.Base(os.Args[0])
-	Short   string = "A brief description of your application"
-	Long    string = `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`
-
+	Short   string = "sparrow 是一个基于 gin 的 restful api 风格的 web 框架"
+	Long    string = `sparrow 是一个简洁优雅的 go 语言 web 框架,用来快速开发 前后端分离 模式下的后端业务逻辑.
+sparrow的目标就是: 尽量平衡自由和规范的界限，既可以让开发者减少开发工作量和开发心智负担，同时开发者有自由组合的权利；当然这是个美好的愿望，希望能实现. 😁😁😁`
 	once sync.Once
 )
+
+type sparrowCmdData struct {
+	config string // 业务配置
+}
 
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
@@ -37,17 +36,20 @@ var RootCmd = &cobra.Command{
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-func InitCmd(use, short, long string) () {
+func InitCmd(use, short, long string) {
 	once.Do(func() {
 		//
 		RootCmd.Use = use
 		RootCmd.Short = short
 		RootCmd.Long = long
-
 		if err := RootCmd.Execute(); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
 	})
+}
+
+// AddCommand 添加子命令
+func AddCommand(cmd ...*cobra.Command) {
+	RootCmd.AddCommand(cmd...)
 }
