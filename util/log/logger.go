2248392f7d97428deecf01c1ca79af78161542c0
guantingwei <guantingwei@sixents.com>
2020-06-30 16:40:45 +0800

    完成 日志编码器和日志配置编码器的获取方式的优化

--- util/log/logger.go
+++ util/log/logger.go
@@ -14,6 +14,54 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+var (
+	// 根据不同的 LoggerConfig.EncoderText 返回不同的生成 zapcore.Encoder 的函数
+	NewEncoderFuncMap = map[string]func(zapcore.EncoderConfig) zapcore.Encoder{
+		EncoderTextJSON:    zapcore.NewJSONEncoder,
+		EncoderTextConsole: zapcore.NewConsoleEncoder,
+	}
+	// 根据不同的 LoggerConfig.EncoderConfigText 返回不同的生成 zapcore.EncoderConfig 的函数
+	NewEncoderConfigFuncMap = map[string]func(*Logger) (zapcore.EncoderConfig, error){
+		EncoderConfigTextDevDefault:  NewEncoderConfigTextDevDefault,
+		EncoderConfigTextDevCustom:   NewEncoderConfigTextDevCustom,
+		EncoderConfigTextProdDefault: NewEncoderConfigTextProdDefault,
+		EncoderConfigTextProdCustom:  NewEncoderConfigTextProdCustom,
+	}
+)
+
+// NewEncoderConfigTextProdDefault   生产环境默认的 EncoderConfig 配置
+func NewEncoderConfigTextProdDefault(l *Logger) (encoderConfig zapcore.EncoderConfig, err error) {
+	encoderConfig = zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.TimeKey = "time"
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
+	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	return
+}
+
+func NewEncoderConfigTextProdCustom(l *Logger) (encoderConfig zapcore.EncoderConfig, err error) {
+	encoderConfig = zap.NewProductionEncoderConfig()
+	err = l.CustomEncoderConfig(&encoderConfig)
+	return
+}
+
+// NewEncoderConfigTextDevDefault   生产环境默认的 EncoderConfig 配置
+func NewEncoderConfigTextDevDefault(l *Logger) (encoderConfig zapcore.EncoderConfig, err error) {
+	encoderConfig = zap.NewDevelopmentEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
+	encoderConfig.EncodeCaller = zapcore.FullCallerEncoder
+	return
+}
+
+func NewEncoderConfigTextDevCustom(l *Logger) (encoderConfig zapcore.EncoderConfig, err error) {
+	encoderConfig = zap.NewDevelopmentEncoderConfig()
+	err = l.CustomEncoderConfig(&encoderConfig)
+	return
+}
+
 type Logger struct {
 	logConfig *LoggerConfig // 配置
 	Logger    *zap.Logger   // 记录器实例
@@ -73,11 +121,11 @@ func (l *Logger) NewZapLogger() (err error) {
 		encoder       zapcore.Encoder
 	)
 	// 获取  zapcore.EncoderConfig
-	if encoderConfig, err = l.logConfig.getEncoderConfig(); err != nil {
+	if encoderConfig, err = l.GetEncoderConfig(); err != nil {
 		return
 	}
 	// 获取 zapcore.Encoder
-	if encoder, err = l.logConfig.getEncoder(encoderConfig); err != nil {
+	if encoder, err = l.GetEncoder(encoderConfig); err != nil {
 		return
 	}
 	if l.logConfig.OutputConsole {
@@ -94,8 +142,8 @@ func (l *Logger) NewZapLogger() (err error) {
 	return
 }
 
-// getFileLogWriter 结合日志切割工具设置日志输出接口
-func (l *Logger) getFileLogWriter(fileConfig *LoggerFileConfig) zapcore.WriteSyncer {
+// GetFileLogWriter 结合日志切割工具设置日志输出接口
+func (l *Logger) GetFileLogWriter(fileConfig *LoggerFileConfig) zapcore.WriteSyncer {
 	lumberJackLogger := &lumberjack.Logger{
 		Filename:   fileConfig.FileName,
 		MaxSize:    fileConfig.MaxSize,
@@ -104,6 +152,63 @@ func (l *Logger) getFileLogWriter(fileConfig *LoggerFileConfig) zapcore.WriteSyn
 		Compress:   fileConfig.Compress,
 	}
 	return zapcore.AddSync(lumberJackLogger)
+}
+
+// GetEncoderConfig 获取  配置编码器 zapcore.EncoderConfig
+func (l *Logger) GetEncoderConfig() (encoderConfig zapcore.EncoderConfig, err error) {
+	funcNewEncoderConfig, ok := NewEncoderConfigFuncMap[l.logConfig.EncoderConfigText]
+	if !ok {
+		err = optionalValuesErr(l.logConfig.EncoderConfigText, "EncoderConfigText")
+	}
+	encoderConfig, err = funcNewEncoderConfig(l)
+	return
+}
+
+// customEncoderConfig 配置自动定义 EncoderConfig
+func (l *Logger) CustomEncoderConfig(encoderConfig *zapcore.EncoderConfig) (err error) {
+	// 配置各种 编码器
+	var EncodeTime zapcore.TimeEncoder
+	if err = EncodeTime.UnmarshalText([]byte(l.logConfig.TimeEncoderText)); err != nil {
+		return
+	}
+	encoderConfig.EncodeTime = EncodeTime
+
+	var EncodeLevel zapcore.LevelEncoder
+	if err = EncodeLevel.UnmarshalText([]byte(l.logConfig.LevelEncoderText)); err != nil {
+		return
+	}
+	encoderConfig.EncodeLevel = EncodeLevel
+
+	var EncodeDuration zapcore.DurationEncoder
+	if err = EncodeDuration.UnmarshalText([]byte(l.logConfig.DurationEncoderText)); err != nil {
+		return
+	}
+	encoderConfig.EncodeDuration = EncodeDuration
+
+	var EncodeCaller zapcore.CallerEncoder
+	if err = EncodeCaller.UnmarshalText([]byte(l.logConfig.CallerEncoderText)); err != nil {
+		return
+	}
+	encoderConfig.EncodeCaller = EncodeCaller
+
+	// 配置各种 key
+	encoderConfig.TimeKey = l.logConfig.TimeKey
+	encoderConfig.LevelKey = l.logConfig.LevelKey
+	encoderConfig.CallerKey = l.logConfig.CallerKey
+	encoderConfig.MessageKey = l.logConfig.MessageKey
+	encoderConfig.StacktraceKey = l.logConfig.StacktraceKey
+	return
+}
+
+// GetEncoder 获取 日志编码器 zapcore.Encoder
+func (l *Logger) GetEncoder(encoderConfig zapcore.EncoderConfig) (encoder zapcore.Encoder, err error) {
+	// 通过 表驱动 方法实现
+	funcNewEncoder, ok := NewEncoderFuncMap[l.logConfig.EncoderText]
+	if !ok {
+		err = optionalValuesErr(l.logConfig.EncoderText, "EncoderText")
+	}
+	encoder = funcNewEncoder(encoderConfig)
+	return
 }
 
 // 2个文件+1个控制台输出
--- util/log/loggerconfig.go
+++ util/log/loggerconfig.go
@@ -9,8 +9,6 @@ package log
 import (
 	"fmt"
 	"github.com/twgcode/sparrow/util/sliceutil"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"path/filepath"
 	"strings"
 )
@@ -82,20 +80,12 @@ var (
 	encoderTextList       = []string{EncoderTextJSON, EncoderTextConsole}
 )
 
-var (
-	//
-	NewEncoderFuncMap = map[string]func(encoderConfig zapcore.EncoderConfig) zapcore.Encoder{
-		EncoderTextJSON:    zapcore.NewJSONEncoder,
-		EncoderTextConsole: zapcore.NewConsoleEncoder,
-	}
-)
-
 func optionalValuesErr(value, class string) (err error) {
 	err = fmt.Errorf("%s not a legal value, please check the optional value of %s", value, class)
 	return
 }
 
-// LoggerConfig 提供日志记录服务; Logger 融合了 zap 和 lumberjack
+// LoggerConfig  提供生成一个 zap.Logger + lumberjack 的配置
 type LoggerConfig struct {
 	OutputConsole       bool // 是否输出到控制台
 	OutputFile          bool // 是否输出到日志文件
@@ -117,7 +107,7 @@ type LoggerConfig struct {
 	StacktraceKey string // 堆栈信息的key
 
 	EncoderText       string // 日志编码器, 用来决定日志记录的整体形式; 有 json 和 console 2 种
-	EncoderConfigText string // 日志编码器配置, An EncoderConfig allows users to configure the concrete encoders supplied by zapcore.
+	EncoderConfigText string // 日志配置编码器, An EncoderConfig allows users to configure the concrete encoders supplied by zapcore.
 }
 
 // simpleFormat 部分字符串类型的字段进行去除空格+转小写的操作,为了让 项目/业务代码更快 run 起来 没使用反射，如果后期压测影响不太这快可用反射减少代码量
@@ -156,101 +146,6 @@ func (l *LoggerConfig) checkEncoderText() (err error) {
 		err = optionalValuesErr(l.EncoderText, "EncoderText")
 		return
 	}
-	return
-}
-func (l *LoggerConfig) getEncoder(encoderConfig zapcore.EncoderConfig) (encoder zapcore.Encoder, err error) {
-	funcNewEncoder, ok := NewEncoderFuncMap[l.EncoderText]
-	if !ok {
-		err = optionalValuesErr(l.EncoderText, "EncoderText")
-	}
-	encoder = funcNewEncoder(encoderConfig)
-	return
-}
-
-// getEncoderConfig 获取  zapcore.EncoderConfig
-func (l *LoggerConfig) getEncoderConfig() (encoderConfig zapcore.EncoderConfig, err error) {
-	switch l.EncoderConfigText {
-	default:
-		err = optionalValuesErr(l.EncoderConfigText, "EncoderConfigText")
-		return
-	case EncoderConfigTextDevCustom:
-		encoderConfig, err = l.EncoderConfigTextDevCustom()
-	case EncoderConfigTextDevDefault:
-		encoderConfig, err = l.EncoderConfigTextDevDefault()
-	case EncoderConfigTextProdCustom:
-		encoderConfig, err = l.EncoderConfigTextProdCustom()
-	case EncoderConfigTextProdDefault:
-		encoderConfig, err = l.EncoderConfigTextProdDefault()
-	}
-	return
-}
-
-// EncoderConfigTextProdDefault   生产环境默认的 EncoderConfig 配置
-func (l *LoggerConfig) EncoderConfigTextProdDefault() (encoderConfig zapcore.EncoderConfig, err error) {
-	encoderConfig = zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	encoderConfig.TimeKey = "time"
-	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
-	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
-	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
-	return
-}
-
-func (l *LoggerConfig) EncoderConfigTextProdCustom() (encoderConfig zapcore.EncoderConfig, err error) {
-	encoderConfig = zap.NewProductionEncoderConfig()
-	err = l.customEncoderConfig(&encoderConfig)
-	return
-}
-
-// EncoderConfigTextDevDefault   生产环境默认的 EncoderConfig 配置
-func (l *LoggerConfig) EncoderConfigTextDevDefault() (encoderConfig zapcore.EncoderConfig, err error) {
-	encoderConfig = zap.NewDevelopmentEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
-	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
-	encoderConfig.EncodeCaller = zapcore.FullCallerEncoder
-	return
-}
-
-func (l *LoggerConfig) EncoderConfigTextDevCustom() (encoderConfig zapcore.EncoderConfig, err error) {
-	encoderConfig = zap.NewDevelopmentEncoderConfig()
-	err = l.customEncoderConfig(&encoderConfig)
-	return
-}
-
-// customEncoderConfig 配置自动定义 EncoderConfig
-func (l *LoggerConfig) customEncoderConfig(encoderConfig *zapcore.EncoderConfig) (err error) {
-	// 配置各种 编码器
-	var EncodeTime zapcore.TimeEncoder
-	if err = EncodeTime.UnmarshalText([]byte(l.TimeEncoderText)); err != nil {
-		return
-	}
-	encoderConfig.EncodeTime = EncodeTime
-
-	var EncodeLevel zapcore.LevelEncoder
-	if err = EncodeLevel.UnmarshalText([]byte(l.LevelEncoderText)); err != nil {
-		return
-	}
-	encoderConfig.EncodeLevel = EncodeLevel
-
-	var EncodeDuration zapcore.DurationEncoder
-	if err = EncodeDuration.UnmarshalText([]byte(l.DurationEncoderText)); err != nil {
-		return
-	}
-	encoderConfig.EncodeDuration = EncodeDuration
-
-	var EncodeCaller zapcore.CallerEncoder
-	if err = EncodeCaller.UnmarshalText([]byte(l.CallerEncoderText)); err != nil {
-		return
-	}
-	encoderConfig.EncodeCaller = EncodeCaller
-
-	// 配置各种 key
-	encoderConfig.TimeKey = l.TimeKey
-	encoderConfig.LevelKey = l.LevelKey
-	encoderConfig.CallerKey = l.CallerKey
-	encoderConfig.MessageKey = l.MessageKey
-	encoderConfig.StacktraceKey = l.StacktraceKey
 	return
 }
 
